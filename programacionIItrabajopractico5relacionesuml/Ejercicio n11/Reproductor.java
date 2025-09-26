a8de4c9b7191691e46445e869bf87cd712339ec5
jcalemis <juancruzalemis@gmail.com>
2025-09-26 01:21:01 -0300

    Create Reproductor.java

---
+++ programacionIItrabajopractico5relacionesuml/Ejercicio n11/Reproductor.java
@@ -0,0 +1,12 @@
+
+package tp5;
+
+
+public class Reproductor {
+    
+    public void reproducir(Cancion cancion) {
+        System.out.println("Reproduciendo: " + cancion.getTitulo());
+        System.out.println("Artista: " + cancion.getArtista().getNombre());
+        System.out.println("Género: " + cancion.getArtista().getGenero());
+    }
+}
